69fce43e5e9a5c620fbb2e964436a799a0375745
Adam Miner <miner.adam1@hotmail.com>
2018-02-20 13:38:05 -0700

    Tweaks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
   var minRandomGen = 19;
   var maxRandomGen = 120;
-
   var minGuess = 1;
   var maxGuess = 12;
+  var wins = 0;
+  var losses = 0;
+  var totalScore = 0;
 
   var gem1 = randomNumberFromRange(minGuess, maxGuess);
   var gem2 = randomNumberFromRange(minGuess, maxGuess);
@@ -12,16 +14,10 @@ $(document).ready(function () {
 
   var randomGen = randomNumberFromRange(minRandomGen, maxRandomGen);
 
-  var wins = 0;
-  var losses = 0;
-
   function randomNumberFromRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  console.log(gem1);
-  console.log(gem2);
-  console.log(gem3);
-  console.log(gem4);
+  
 
 })
